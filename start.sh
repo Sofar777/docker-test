ede5c0b5eac8956ff627d4e4db76b6dfca04a7fb
Mr.Sofar <315520536@qq.com>
2018-07-25 12:42:55 +0800

    优化 start 脚本

--- start.sh
+++ start.sh
@@ -8,13 +8,17 @@
 # 		c、运行镜像
 #!/bin/bash
 containerID=`docker ps -a | grep $1 | awk '{print $1}'`
-divideTag='\n===================\n\n'
+divideTag='\n===================\n'
 echo  "构建镜像名称: $1"
+consoleLog(){
+	echo -e "${divideTag}$1${divideTag}"
+}
 
 # 拉取代码
 git pull
-echo -e "拉取代码完毕${divideTag}"
+consoleLog "拉取代码完毕"
 	
+
 # 如果进程存在	
 # 	杀死进程
 # 	移除镜像
@@ -24,14 +28,14 @@ if [ -n "$containerID" ]; then
     docker rm ${containerID}
     imageID=`docker images | grep $1 | awk '{print $3}'`
     docker rmi ${imageID}
-    echo -e "旧进程清除完毕${divideTag}"
+    consoleLog "旧进程清除完毕"
 fi
 
 # 构建镜像
 docker build -t $1 .
-echo -e "构建镜像完毕${divideTag}"
+consoleLog "构建镜像完毕"
 
 # 运行镜像
 docker run -d -p 80:3000 $1
-echo -e "镜像运行成功${divideTag}"
+consoleLog "镜像运行成功"
 
