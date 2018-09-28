b7af3758eb9d8e65ddc830511bde312be83cbcf6
Mr.Sofar <315520536@qq.com>
2018-09-28 20:55:58 +0800

    add shelljs

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
+var shell = require('shelljs');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/teme', function(req, res, next) {
+  	res.render('index', { title: 'Express' });
 });
-
+router.post('/res', function(req, res, next) {
+  	shell.exec("sh ../start.sh nodejs");
+  	res.json({
+  		msg: "success"
+  	})
+});
 module.exports = router;
