fc41363bfcdf7998fe4bea7aa13dd47f6a0b7956
Mr.Sofar <315520536@qq.com>
2018-10-16 18:46:48 +0800

    init

--- routes/index.js
+++ routes/index.js
@@ -1,26 +1,9 @@
 var express = require('express');
 var router = express.Router();
-var shell = require('shelljs');
 
 /* GET home page. */
-router.get('/teme', function(req, res, next) {
-  	res.render('index', { title: 'Express' });
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
 });
-router.post('/res', function(req, res, next) {
-	if (shell.exec('sh ' + __dirname +'/../start.sh nodejs').code !== 0) {
-	  	console.log("1")
-	  	shell.exit(1);
-	  	res.json({
-	  		msg: "fail"
-	  	})
-	}else {
-	  	console.log("2")
 
-		res.json({
-	  		msg: "success"
-	  	})
-	}
-  	
-  	
-});
 module.exports = router;
